0117222e5d6cf2915147fa2d779202d33082b01b
lempire123 <61431140+lempire123@users.noreply.github.com>
2022-08-30 18:48:34 +0200

    polishing

--- test/foundry/Regular_Tests/Fuzzing.t.sol
+++ test/foundry/Regular_Tests/Fuzzing.t.sol
@@ -1,95 +1,247 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import "./jetStakingV2.t.sol";
+import "./Setup.sol";
+
+/**
+ * @title JetStakingV1 foundry fuzzing tests
+ * @author Lance Henderson
+ *
+ * @dev Fuzzing test suite for JetStakingV1 written in solidity using Foundry
+ * 
+ *      FuzzingTests implements a testing technique known  as "fuzzing". This technique 
+ *      involves testing a specific scenario with a wide array of test values, with the 
+ *      goal of finding values which create unexepcted behaviour in the system.
+ *    
+ *      A detailed description of  what each test does is provided in  the space  above 
+ *      the test itself.
+ */
 
 contract FuzzingTests is Setup {
 
-    function testStakeAmount(uint256 x) public {
-        vm.assume(x < aurora.balanceOf(address(this)));
+    /*/////////////////////////////////////////////////////////////
+                    STAKE / UNSTAKE (PARAM: AMOUNT)
+    /////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Stake and immediately unstake from the jetStaking contract. No reward 
+     *      tokens nor aurora tokens should accrue. This is checked in the assert.
+     *
+     * @param x Amount of aurora tokens to stake
+     */
+    function testStakeAndWithdraw(uint256 x) public {
+        vm.assume(x > 0 && x < maxSupply);
+        uint256 balanceBefore = aurora.balanceOf(address(this));
         aurora.approve(address(jetStaking), x);
         jetStaking.stake(x);
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        jetStaking.withdrawAll();
+        assert(balanceBefore == aurora.balanceOf(address(this)));
     }
 
-    // Returns: too many global rejects
-    // function testStakeExceedBalance(uint256 x) public {
-    //     vm.assume(x > aurora.balanceOf(address(this)));
-    //     vm.assume(x < aurora.totalSupply());
-    //     aurora.approve(address(jetStaking), x);
-    //     vm.expectRevert("ERC20: transfer amount exceeds balance");
-    //     jetStaking.stake(x);
-    // }
-
-    function testStakeAddress(address x) public {
-        vm.assume(x != address(0));
-        deal(address(aurora), x, 100 ether);
-        vm.startPrank(x);
-        aurora.approve(address(jetStaking), 100 ether);
-        jetStaking.stake(100 ether);
+    /**
+     * @dev Stake and unstake after waiting 1 week from the jetStaking contract. Due
+     *      to the one week waiting time rewards should have accrued.
+     *
+     * @param x Amount of aurora tokens to stake
+     */
+    function testStakeWaitAndWithdraw(uint256 x) public {
+        vm.assume(x > 0 && x < maxSupply);
+        uint256 balanceBefore = aurora.balanceOf(address(this));
+        aurora.approve(address(jetStaking), x);
+        jetStaking.stake(x);
+        vm.warp(block.timestamp + 1 weeks);
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        jetStaking.withdrawAll();
+        assert(balanceBefore < aurora.balanceOf(address(this)));
     }
 
-    function testStakeWithdrawTime(uint256 x) public {
-        vm.assume(x < 315360000); // seconds in 10 years
-        aurora.approve(address(jetStaking), 100 ether);
-        jetStaking.stake(100 ether);
+    /**
+     * @dev Stake and unstake after waiting 1 week from the jetStaking contract. Batch
+     *      withdraw allows withdraw several stream tokens in one transaction. Here we
+     *      are testing the withdrawal of two streams (0 & 1), and making sure our balance
+     *      of both tokens has increased since the time of staking.
+     *
+     * @param x Amount of aurora tokens to stake
+     */
+    function testStakeAndBatchWithdraw(uint256 x) public {
+        vm.assume(x > 0 && x < maxSupply);
+        uint256 balanceBefore = aurora.balanceOf(address(this));
+        uint256 rewardBalance = rewardToken1.balanceOf(address(this));
+        aurora.approve(address(jetStaking), x);
+        jetStaking.stake(x);
+        vm.warp(block.timestamp + 1 weeks);
+        jetStaking.moveAllRewardsToPending();
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        uint256[] memory streamId = new uint256[](2);
+        streamId[0] = 0;
+        streamId[1] = 1;
+        jetStaking.batchWithdraw(streamId);
+        assert(balanceBefore < aurora.balanceOf(address(this)));
+    }
+
+    /*/////////////////////////////////////////////////////////////
+                    STAKE / UNSTAKE (PARAM: TIME)
+    /////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Stake and unstake after waiting x time from the jetStaking contract. Due
+     *      to the waiting time rewards should have accrued.
+     *
+     * @param x Number of seconds to wait before unstaking
+     */
+    function testStakeWaitAndWithdraw2(uint256 x) public {
+        uint256 amount = 100 ether;
+        vm.assume(x > 0 && x < 315360000); // seconds in 10 years
+        uint256 balanceBefore = aurora.balanceOf(address(this));
+        aurora.approve(address(jetStaking), amount);
+        jetStaking.stake(amount);
         vm.warp(block.timestamp + x);
         jetStaking.unstakeAll();
         vm.warp(block.timestamp + 2 days + 1);
         jetStaking.withdrawAll();
+        if(x > 50) {
+             assert(balanceBefore < aurora.balanceOf(address(this)));
+        }
     }
 
-    function testStakeWithdrawAmount(uint256 x) public {
-        vm.assume(x < 100 ether);
-        vm.assume(x != 0);
-        aurora.approve(address(jetStaking), 100 ether);
-        jetStaking.stake(100 ether);
-        jetStaking.unstake(x);
-        vm.warp(block.timestamp + 2 days + 1);
-        jetStaking.withdrawAll();
-    }
-
-    function testProfitSeeker(uint256 x) public {
-        uint256 minProfitMargin = 10**17;
+    /**
+     * @dev Stake and unstake after waiting x time from the jetStaking contract. Batch
+     *      withdraw allows withdraw several stream tokens in one transaction. Here we
+     *      are testing the withdrawal of two streams (0 & 1), and making sure our balance
+     *      of both tokens has increased since the time of staking.
+     *
+     * @param x Number of seconds to wait before unstaking
+     */
+    function testStakeAndBatchWithdraw2(uint256 x) public {
+        uint256 amount = 100 ether;
+        vm.assume(x > 0 && x < 315360000); // seconds in 10 years
         uint256 balanceBefore = aurora.balanceOf(address(this));
-        vm.assume(x < balanceBefore);
-        vm.assume(x > 0);
-        aurora.approve(address(jetStaking), x);
-        jetStaking.stake(x);
+        uint256 rewardBalance = rewardToken1.balanceOf(address(this));
+        aurora.approve(address(jetStaking), amount);
+        jetStaking.stake(amount);
+        vm.warp(block.timestamp + x);
+        jetStaking.moveAllRewardsToPending();
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        uint256[] memory streamId = new uint256[](2);
+        streamId[0] = 0;
+        streamId[1] = 1;
+        jetStaking.batchWithdraw(streamId);
+        if(x > 50) {
+             assert(balanceBefore < aurora.balanceOf(address(this)));
+        }
+    }
+
+    /*/////////////////////////////////////////////////////////////
+                    STAKE / UNSTAKE (PARAM: ADDRESS)
+    /////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Stake and immediately unstake from the jetStaking contract. No reward 
+     *      tokens nor aurora tokens should accrue. This is checked in the assert.
+     *
+     * @param x Address that will stake aurora tokens
+     */
+    function testStakeAndWithdraw3(address x) public {
+        uint256 amount = 100 ether;
+        vm.assume(x != address(0));
+        stakeFor(x, amount);
+        vm.startPrank(x);
         jetStaking.unstakeAll();
         vm.warp(block.timestamp + 2 days + 1);
         jetStaking.withdrawAll();
-        uint256 balanceAfter = aurora.balanceOf(address(this));
-        assert(balanceAfter < balanceBefore + minProfitMargin);
+        assert(amount == aurora.balanceOf(x));
     }
 
-    function testUpdateTreasury(address t) public {
-        vm.assume(t != address(0));
-        jetStaking.adminPause(1);
-        jetStaking.updateTreasury(t);
+    /**
+     * @dev Stake and unstake after waiting 1 week from the jetStaking contract. Due
+     *      to the one week waiting time rewards should have accrued.
+     *
+     * @param x Address that will stake aurora tokens
+     */
+    function testStakeWaitAndWithdraw3(address x) public {
+        uint256 amount = 100 ether;
+        vm.assume(x != address(0));
+        stakeFor(x, amount);
+        vm.startPrank(x);
+        vm.warp(block.timestamp + 1 weeks);
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        jetStaking.withdrawAll();
+        assert(amount < aurora.balanceOf(x));
     }
 
-    function testStreamManagerRole() public {
-        deal(address(rewardToken1), address(this), 100 ether);
-        rewardToken1.approve(address(jetStaking), 100 ether);
-        aurora.approve(address(jetStaking), 1000 ether);
-        uint256[] memory scheduleTimes = new uint256[](2);
-        scheduleTimes[0] = block.timestamp + 1000;
-        scheduleTimes[1] = block.timestamp + 2000;
-        uint256[] memory scheduleRewards = new uint256[](2);
-        scheduleRewards[0] = 1000;
-        scheduleRewards[1] = 0;
-        jetStaking.proposeStream(
-            user1,
-            address(rewardToken1),
-            100,
-            1000,
-            100,
-            scheduleTimes,
-            scheduleRewards,
-            1 days
-            );
+    /**
+     * @dev Stake and unstake after waiting 1 week from the jetStaking contract. Batch
+     *      withdraw allows withdraw several stream tokens in one transaction. Here we
+     *      are testing the withdrawal of two streams (0 & 1), and making sure our balance
+     *      of both tokens has increased since the time of staking.
+     *
+     * @param x Address that will stake aurora tokens
+     */
+    function testStakeAndBatchWithdraw3(address x) public {
+        uint256 amount = 100 ether;
+        vm.assume(x != address(0));
+        stakeFor(x, amount);
+        vm.startPrank(x);
+        vm.warp(block.timestamp + 1 weeks);
+        jetStaking.moveAllRewardsToPending();
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        uint256[] memory streamId = new uint256[](2);
+        streamId[0] = 0;
+        streamId[1] = 1;
+        jetStaking.batchWithdraw(streamId);
+        assert(amount < aurora.balanceOf(x));
     }
 
+    /*/////////////////////////////////////////////////////////////
+                STAKE / UNSTAKE (PARAM: AMOUNT+TIME+ADDRESS)
+    /////////////////////////////////////////////////////////////*/
 
+    /**
+     * @dev Stake and immediately unstake from the jetStaking contract. No reward 
+     *      tokens nor aurora tokens should accrue. This is checked in the assert.
+     *
+     * @param x Amount of aurora tokens to stake
+     * @param y Number of seconds to wait before unstaking
+     * @param z Address that will stake aurora tokens
+     */
+    function testStakeAndWithdraw4(uint256 x, uint256 y, address z) public {
+        vm.assume(x > 0 && x < maxSupply);
+        vm.assume(y > 0 && y < 315360000); // seconds in 10 years
+        vm.assume(z != address(0));
+        stakeFor(z, x);
+        vm.startPrank(z);
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        jetStaking.withdrawAll();
+        assert(x == aurora.balanceOf(z));
+    }
 
+    /**
+     * @dev Stake and unstake after waiting y time from the jetStaking contract. Due
+     *      to the waiting time rewards should have accrued.
+     *
+     * @param x Amount of aurora tokens to stake
+     * @param y Number of seconds to wait before unstaking
+     * @param z Address that will stake aurora tokens
+     */
+    function testStakeWaitAndWithdraw4(uint256 x, uint256 y, address z) public {
+        vm.assume(x > 0 && x < maxSupply);
+        vm.assume(y > 0 && y < 315360000); // seconds in 10 years
+        vm.assume(z != address(0));
+        uint256 balanceBefore = aurora.balanceOf(z);
+        stakeFor(z, x);
+        vm.startPrank(z);
+        vm.warp(block.timestamp + y);
+        jetStaking.unstakeAll();
+        vm.warp(block.timestamp + 2 days + 1);
+        jetStaking.withdrawAll();
+        assert(balanceBefore < aurora.balanceOf(z));
+    }
 }
--- test/foundry/Regular_Tests/Setup.sol
+++ test/foundry/Regular_Tests/Setup.sol
@@ -3,17 +3,20 @@ pragma solidity ^0.8.10;
 
 import "../Contracts/F_JetStakingV1.sol";
 import "../Contracts/F_Treasury.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 import "forge-std/Test.sol";
 
-/// @title JetStaking Foundry Tests
-/// @author Lance Henderson
-/// 
-/// @notice The following contract provides a suite of tests for JetStaking.sol
-/// contract. Further detail of what each tests does is provided above the actual
-/// test code.
+/**
+ * @title JetStakingV1 foundry tests
+ * @author Lance Henderson
+ *
+ * @dev The following contract, "Setup", takes care of the initialization of the environment
+ *      needed to test the jetStaking contracts. This includes setting up state variables, and
+ *      deploying a new version of the jetStaking contract (locally).
+ */
+
 
 contract Setup is Test {
     // JetStaking 
@@ -40,28 +43,34 @@ contract Setup is Test {
     address user1 = address(7);
     address user2 = address(8);
     address user3 = address(9);
-
+    // Handy Variables
+    uint256 maxSupply = 10000000000 ether;
     // Code below is executed prior to every test
     function setUp() public {
+        // Create new instance of the treasury
         treasury = new Treasury();
+        // Add each reward token to an array
         rewardTokens.push(address(aurora));
         rewardTokens.push(address(rewardToken1));
         rewardTokens.push(address(rewardToken2));
         rewardTokens.push(address(rewardToken3));
+        // Initialize treasury + jetStaking
         treasury.initialize(rewardTokens, flags);
         jetStaking = new JetStakingV1();
         uint256 startTime = block.timestamp + 10;
+        // Schedule time for aurora stream
         scheduleTimes.push(startTime);
         scheduleTimes.push(startTime + 1 * oneYear);
         scheduleTimes.push(startTime + 2 * oneYear);
         scheduleTimes.push(startTime + 3 * oneYear);
         scheduleTimes.push(startTime + 4 * oneYear);
+        // Schedule rewards for aurora stream
         scheduleRewards.push(200000000 ether);
         scheduleRewards.push(100000000 ether);
         scheduleRewards.push(50000000 ether);
         scheduleRewards.push(25000000 ether);
         scheduleRewards.push(0 ether);
-
+        // Initializing jetStaking
         jetStaking.initialize(
             address(aurora), 
             streamOwner, 
@@ -73,30 +82,32 @@ contract Setup is Test {
             maxWeight, 
             minWeight);
         // 'Deal' - cheatcode provided by Foundry to set the balance of a token of a user
-        deal(address(aurora), address(this), 10000 ether); 
-        deal(address(aurora), address(treasury), 100000000000 ether); 
+        deal(address(aurora), address(this), maxSupply); 
+        deal(address(aurora), address(treasury), maxSupply); 
         deal(address(rewardToken1), address(treasury), 100000000000 ether); 
         deal(address(rewardToken2), address(treasury), 100000000000 ether); 
         deal(address(rewardToken3), address(treasury), 100000000000 ether); 
+        // Fetching all roles from jetStaking
         bytes32 defaultAdminRole = jetStaking.DEFAULT_ADMIN_ROLE();
         bytes32 claimRole = jetStaking.CLAIM_ROLE();
         bytes32 airdropRole = jetStaking.AIRDROP_ROLE();
         bytes32 pauseRole = jetStaking.PAUSE_ROLE();
         bytes32 streamManagerRole = jetStaking.STREAM_MANAGER_ROLE();
+        // Granting all role access to address(this)
         jetStaking.grantRole(claimRole, address(this));
         jetStaking.grantRole(airdropRole, address(this));
         jetStaking.grantRole(pauseRole, address(this));
         jetStaking.grantRole(streamManagerRole, address(this));
-
+        // Grant default adming role to jetStaking address
         treasury.grantRole(defaultAdminRole, address(jetStaking));
         vm.startPrank(address(treasury));
         aurora.approve(address(jetStaking), 2**256 - 1);
         vm.stopPrank();
     } 
 
-    /* ===================================
-                    HELPER
-    ==================================== */
+    /*/////////////////////////////////////////////////////////////
+                                HELPER
+    /////////////////////////////////////////////////////////////*/
 
     // @notice Helper function to stake aurora tokens 
     // @param account Address of account to stake for
